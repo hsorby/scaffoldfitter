86f6b0599bd07ed2234cb85bfaa1421563600a64
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-12-04 10:29:34 +1300

    Set version to 0.8.3.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ source_license = readfile("LICENSE")
 
 setup(
     name="scaffoldfitter",
-    version="0.8.2",
+    version="0.8.3",
     description="Scaffold/model geometric fitting library using Zinc.",
     long_description="\n".join(readme) + source_license,
     long_description_content_type="text/x-rst",
