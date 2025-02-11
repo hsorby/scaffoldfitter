47b852e72ec126663d24a28e3f6b3e8ceb18b68e
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-02-12 11:26:26 +1300

    Update to changes in API to zinc.utils.

--- src/scaffoldfitter/fitter.py
+++ src/scaffoldfitter/fitter.py
@@ -6,10 +6,9 @@ import json
 
 from cmlibs.maths.vectorops import add, mult, sub
 from cmlibs.utils.zinc.field import assignFieldParameters, createFieldFiniteElementClone, getGroupList, \
-    findOrCreateFieldFiniteElement, findOrCreateFieldStoredMeshLocation, getUniqueFieldName, orphanFieldByName
-from cmlibs.utils.zinc.finiteelement import evaluate_field_nodeset_range, findNodeWithName
+    findOrCreateFieldFiniteElement, findOrCreateFieldStoredMeshLocation, getUniqueFieldName, orphanFieldByName, create_jacobian_determinant_field
+from cmlibs.utils.zinc.finiteelement import evaluate_field_nodeset_range, findNodeWithName, get_scalar_field_minimum_in_mesh
 from cmlibs.utils.zinc.general import ChangeManager
-from cmlibs.utils.zinc.field import get_element_jacobian_field, get_scalar_field_minimum_in_mesh
 from cmlibs.utils.zinc.region import write_to_buffer, read_from_buffer
 from cmlibs.zinc.context import Context
 from cmlibs.zinc.element import Elementbasis, Elementfieldtemplate
@@ -864,7 +863,7 @@ class Fitter:
         :return: Element identifier, minimum jacobian value. Values are -1, inf if there is no data or bad fields.
         """
         with ChangeManager(self._fieldmodule):
-            jacobian = get_element_jacobian_field(self._modelCoordinatesField)
+            jacobian = create_jacobian_determinant_field(self._modelCoordinatesField)
             result = get_scalar_field_minimum_in_mesh(jacobian, mesh_group)
             del jacobian
 
